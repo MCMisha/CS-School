159944ac62d194e666ec9a4a29ca702b9c57cf55
misha <misha.yakushevich4@gmail.com>
2024-10-10 11:40:58 +0200

    HT1 Completed all tasks for first homework

---
+++ ConsoleAppHT1_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleAppHW1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static string ConvertIntToDuodecimal(int intNumber)
+        {
+            string result = string.Empty;
+            const string digits = "0123456789AB";
+            
+            while (intNumber > 0)
+            {
+                result = digits[intNumber % 12] + result;
+                intNumber /= 12;
+            }
+
+            return result == string.Empty ? "0" : result;
+        }
+        
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter two integers: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            for (int i = a; i < b + 1; i++)
+            {
+                string duodecimal = ConvertIntToDuodecimal(i);
+                if (duodecimal.Count(ch => ch == 'A') == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleAppHT1_3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleAppHW1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elements in the array: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            Console.WriteLine("Enter the elements of the array:");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Original array:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            
+            int[] uniqueArray = new int[n];  // Maximum size could be n
+            int uniqueCount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                bool isUnique = true;
+                
+                for (int j = 0; j < uniqueCount; j++)
+                {
+                    if (array[i] == uniqueArray[j])
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                
+                if (isUnique)
+                {
+                    uniqueArray[uniqueCount] = array[i];
+                    uniqueCount++;
+                }
+            }
+            
+            Console.WriteLine("Array with unique elements:");
+            for (int i = 0; i < uniqueCount; i++)
+            {
+                Console.Write(uniqueArray[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
