7dc65c33fc5a8c631806d2688f40d56ac874ecf7
misha <misha.yakushevich4@gmail.com>
2024-10-21 17:54:53 +0200

    HT3.1 Completed task

---
+++ ConsoleAppHT3_1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleAppHT3_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            IQueue<int> queueWithInt = new Queue<int>();
+            IQueue<(uint, uint)> queueWithPairsOfUint = new Queue<(uint, uint)>();
+            IQueue<double> queueWithDouble = new Queue<double>();
+            
+            queueWithInt.Enqueue(10);
+            queueWithInt.Enqueue(20);
+            queueWithInt.Enqueue(30);
+            
+            queueWithPairsOfUint.Enqueue((10, 20));
+            queueWithPairsOfUint.Enqueue((30, 40));
+            queueWithPairsOfUint.Enqueue((50, 60));
+
+            queueWithDouble.Enqueue(10.2);
+            queueWithDouble.Enqueue(20.4);
+            queueWithDouble.Enqueue(30.6);
+            IQueue<double> tail = queueWithDouble.Tail();
+            
+            Console.WriteLine("Queue with integers:");
+            while (!queueWithInt.IsEmpty())
+            {
+                Console.Write($"{queueWithInt.Dequeue()} ");
+            }
+            Console.WriteLine();
+            
+            Console.WriteLine("Queue with pairs of unsigned integers:");
+            while (!queueWithPairsOfUint.IsEmpty())
+            {
+                (uint first, uint second) = queueWithPairsOfUint.Dequeue();
+                Console.Write($"({first}, {second}) ");
+            }
+            Console.WriteLine();
+            
+            Console.WriteLine("Queue with doubles(tail):");
+            while (!tail.IsEmpty())
+            {
+                Console.Write($"{tail.Dequeue()} ");
+            }
+        }
+    }
+}
